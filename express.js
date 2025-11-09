3c1912416d0cff1032b16370d21d2d745bc5da7f
portifolio <matheushsp100@gmail.com>
2025-11-09 14:39:30 -0300

    x

--- express.js
+++ express.js
@@ -66,4 +66,6 @@ app.get("/api/produtos", async (req, res) => {
 });
 
 
-app.listen(3000, ()=> console.log("API rodando em http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
+
